575a8d5ead73e24912422837bdc70b081de2693e
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-08-19 19:47:54 -0700

    Add project files.

---
+++ Strongly Connected Components Kosaraju's/Graph.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strongly_Connected_Components_Kosaraju_s
+{
+    class Graph
+    {
+        int nodes;
+        List<List<int>> adj = new List<List<int>>();
+        List<List<int>> rev = new List<List<int>>();
+
+        public Graph(int N)
+        {
+            this.nodes = N;
+            for (int i = 0; i < nodes; i++)
+            {
+                adj.Add(new List<int>());
+                rev.Add(new List<int>());
+            }
+        }
+
+        public void AddEdge(int Source, int Destination)
+        {
+            adj[Source].Add(Destination);
+            rev[Destination].Add(Source);
+        }
+
+        public void SCC(int StartNode)
+        {
+            bool[] visited = new bool[nodes];
+            Stack<int> S = new Stack<int>();
+
+
+            for (int i = 0; i < nodes; i++)
+            {
+                if (!visited[i])
+                    SCCUtil(visited, S, StartNode);
+            }
+
+            for (int i = 0; i < nodes; i++)
+            {
+                visited[i] = false;
+            }
+
+            while(S.Count != 0)
+            {
+                Queue<int> Q = new Queue<int>();
+                int startRev = S.Pop();
+                if(!visited[startRev])
+                    RevUtil(visited, startRev, Q, S);
+            }
+        }
+
+        public void RevUtil(bool[] visited,int RevNode,  Queue<int> Q, Stack<int> S)
+        {
+            Q.Enqueue(RevNode);
+            visited[RevNode] = true;
+
+            foreach (var child in rev[RevNode])
+            {
+                if (!visited[child])
+                {
+                    RevUtil(visited, child, Q, S);
+                    return;
+                }
+            }
+            Console.WriteLine(string.Join(",", Q));
+        }
+
+        public void SCCUtil(bool[] visited, Stack<int> S, int StartedNode)
+        {
+            visited[StartedNode] = true;
+
+            foreach (var child in adj[StartedNode])
+            {
+                if (!visited[child])
+                {
+                    SCCUtil(visited, S, child);
+                }
+            }
+            S.Push(StartedNode);
+        }
+    }
+}
---
+++ Strongly Connected Components Kosaraju's/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Strongly_Connected_Components_Kosaraju_s
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph G = new Graph(4);
+
+            G.AddEdge(0, 1);
+            G.AddEdge(1, 2);
+            G.AddEdge(2, 0);
+            G.AddEdge(0, 3);
+
+            G.SCC(0);
+        }
+    }
+}
